e146d1b562d2bd5ea66bf021329552e11a62c6ce
kezagiselle <kezagiselle776@gmail.com>
2024-02-21 16:32:28 +0200

    weather project

---
+++ wealther projec.js
@@ -0,0 +1,9 @@
+//creating a variable called kelvin
+const kelvin =0;
+//converting to celsius
+const celsius = kelvin - 273;
+//using the fahrenheit equestion
+ let fahrenheit = celsius * ( 9 / 5) + 32;
+ // using math.floor() to round down to fahrenheit
+ fahrenheit = Math.floor(fahrenheit);
+ console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`);
